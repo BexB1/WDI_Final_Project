68f4b76d8ac52c9d845392dca4efa45429ceb584
Bex B <bexb1@hotmail.com>
2016-09-09 11:21:28 +0100

    Updated mongodb uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/londate",
   development: "mongodb://localhost/londate",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/londate"
+  production: process.env.MONGODB_URI || "mongodb://localhost/londate"
 }
 
 module.exports = function(env) {
